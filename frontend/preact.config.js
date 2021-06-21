fd30e56019cb114ec25254b9c4862d0bdda62ae3
mosm <airman515@gmail.com>
2021-06-21 13:52:27 +0900

    frontend: fix resource paths for PWA

--- frontend/preact.config.js
+++ frontend/preact.config.js
@@ -3,12 +3,9 @@ import envVars from 'preact-cli-plugin-env-vars';
 export default function (config, env, helpers) {
     envVars(config, env, helpers);
 
-    const publicPath = process.env.GITHUB_PAGES
-        ? `/${process.env.GITHUB_PAGES}/`
-        : '/';
-    const ghEnv =
-        process.env.GITHUB_PAGES &&
-        JSON.stringify(`${process.env.GITHUB_PAGES}`);
+    const githubPages = process.env.GITHUB_PAGES;
+    const publicPath = githubPages ? `/${githubPages}/` : '/';
+    const ghEnv = process.env.GITHUB_PAGES && JSON.stringify(`${githubPages}`);
 
     config.output.publicPath = publicPath;
     const { plugin } = helpers.getPluginsByName(config, 'DefinePlugin')[0];
